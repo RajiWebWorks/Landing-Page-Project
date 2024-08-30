9cc91d59991dc913230b1d18cdc0730f92e5b494
Rajeshwari Kumar <rajirdx39@gmail.com>
2024-08-30 18:57:43 +0530

    Add files via upload

---
+++ Landing Page Project/timer.js
@@ -0,0 +1,23 @@
+
+  
+var countDownDate = new Date("Jun 5, 2022 15:37:25").getTime();
+ 
+var x = setInterval(function() {
+ 
+  var now = new Date().getTime();
+ 
+  var distance = countDownDate - now;
+ 
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+ 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "45d 4hr 11m 25s";
+  }
+}, 1000); 
